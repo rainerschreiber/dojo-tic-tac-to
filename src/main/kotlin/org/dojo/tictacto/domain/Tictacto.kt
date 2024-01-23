deaff44ba6ec0ae57b4eafa14885a2c6e07d1115
Rainer Schreiber <rainer.schreiber@ns.nl>
2024-01-23 20:16:30 +0100

    Added check for played positions to play method

--- src/main/kotlin/org/dojo/tictacto/domain/Tictacto.kt
+++ src/main/kotlin/org/dojo/tictacto/domain/Tictacto.kt
@@ -6,7 +6,9 @@ class Tictacto {
 
     fun play(position: Int, player: Player) {
         require(position in 1..9) { "Position must be between 1 and 9" }
+        require(board[position - 1] == " ") { "Position $position is already played" }
         board[position - 1] = player.symbol
     }
+
 
 }
--- src/test/kotlin/org/dojo/tictacto/domain/TictactoPlayTest.kt
+++ src/test/kotlin/org/dojo/tictacto/domain/TictactoPlayTest.kt
@@ -1,18 +1,18 @@
 package org.dojo.tictacto.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import kotlin.test.Test
 import kotlin.test.assertFailsWith
 
-class TictactoTest {
+class TictactoPlayTest {
 
     private val testPlayer = Player("Test player", "X")
 
     @ParameterizedTest
     @ValueSource(ints = [0, -1, -2, -5, -20, -100, -1000, Integer.MIN_VALUE])
-    fun `Should throw an exception when play the game with position values 0 or lower`(position: Int) {
+    fun `Should throw an exception when the game is played with a position value 0 or lower`(position: Int) {
         val tictacto = Tictacto()
         assertFailsWith<IllegalArgumentException>(
             message = "A call to Game.play method with position value 0 or lower should throw an IllegalArgumentException",
@@ -87,10 +87,23 @@ class TictactoTest {
 
     @ParameterizedTest
     @ValueSource(ints = [10, 11, 12, 15, 20, 100, 1000, Integer.MAX_VALUE])
-    fun `Should throw an exception when play the game with position values 10 or higher`(position: Int) {
+    fun `Should throw an exception when the game is played with a position value 10 or higher`(position: Int) {
         val tictacto = Tictacto()
         assertFailsWith<IllegalArgumentException>(
             message = "A call to Game.play method with position value 10 or higher should throw an IllegalArgumentException",
+            block = {
+                tictacto.play(position, testPlayer)
+            }
+        )
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1,2,3,4,5,6,7,8,9])
+    fun `Should throw an exception when a player tries to play a position that is already played`(position: Int) {
+        val tictacto = Tictacto()
+        tictacto.play(position, testPlayer)
+        assertFailsWith<IllegalArgumentException>(
+            message = "A call to Game.play method with a position that is already played should throw an IllegalArgumentException",
             block = {
                 tictacto.play(position, testPlayer)
             }
